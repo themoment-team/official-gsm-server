28f036d555601600c6483e5665082975d985adb4
신희성 <vvwv3092@gmail.com>
2023-12-29 12:02:04 +0900

    perf UserNameModifyRequest validation

--- presentation/src/main/java/team/themoment/officialgsm/admin/auth/dto/request/UserNameModifyRequest.java
+++ presentation/src/main/java/team/themoment/officialgsm/admin/auth/dto/request/UserNameModifyRequest.java
@@ -1,5 +1,6 @@
 package team.themoment.officialgsm.admin.auth.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -9,6 +10,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserNameModifyRequest {
+    @NotBlank
     @Pattern(regexp = "^.{2,5}$")
     private String userName;
 }
