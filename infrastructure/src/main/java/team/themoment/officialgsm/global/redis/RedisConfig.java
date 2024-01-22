8dae3270fc080905ea4083b543fb2089358f0c1c
신희성 <vvwv3092@gmail.com>
2024-01-22 09:39:46 +0900

    chore remove ZSetOperations

--- infrastructure/src/main/java/team/themoment/officialgsm/global/redis/RedisConfig.java
+++ infrastructure/src/main/java/team/themoment/officialgsm/global/redis/RedisConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializer;
@@ -35,11 +34,6 @@ public class RedisConfig {
         redisTemplate.setKeySerializer(RedisSerializer.string());
         redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
         return redisTemplate;
-    }
-
-    @Bean
-    ZSetOperations<String, Object> zSetOperations() {
-        return redisTemplate().opsForZSet();
     }
 }
 
