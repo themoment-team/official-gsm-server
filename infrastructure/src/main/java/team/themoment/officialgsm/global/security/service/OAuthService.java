e9bfe07b60a3db4d9b43907daa8c794791dac557
신희성 <vvwv3092@gmail.com>
2024-01-22 13:51:08 +0900

    feat redirect method

--- infrastructure/src/main/java/team/themoment/officialgsm/global/security/service/OAuthService.java
+++ infrastructure/src/main/java/team/themoment/officialgsm/global/security/service/OAuthService.java
@@ -1,17 +1,17 @@
 package team.themoment.officialgsm.global.security.service;
+
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
-import static team.themoment.officialgsm.domain.user.Role.*;
-
 import team.themoment.officialgsm.common.util.ConstantsUtil;
 import team.themoment.officialgsm.common.util.CookieUtil;
 import team.themoment.officialgsm.common.util.EmailUtil;
@@ -22,14 +22,19 @@ import team.themoment.officialgsm.persistence.user.entity.UserJpaEntity;
 import team.themoment.officialgsm.persistence.user.repository.UserJpaRepository;
 import team.themoment.officialgsm.repository.token.RefreshTokenRepository;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+import static team.themoment.officialgsm.domain.user.Role.ADMIN;
+import static team.themoment.officialgsm.domain.user.Role.UNAPPROVED;
+
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class OAuthService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
 
     private final OAuth2UserService<OAuth2UserRequest, OAuth2User> delegateOauth2UserService = new DefaultOAuth2UserService();
@@ -42,6 +47,9 @@ public class OAuthService implements OAuth2UserService<OAuth2UserRequest, OAuth2
 
     @Value("${domain}")
     private String schoolDomain;
+
+    @Value("${site-address}")
+    private String siteAddress;
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
@@ -67,6 +75,8 @@ public class OAuthService implements OAuth2UserService<OAuth2UserRequest, OAuth2
         authorities.add(new SimpleGrantedAuthority(role.name()));
 
         cookieLogic(user);
+
+        redirectUser(user);
 
         return new UserInfo(authorities, attributes, nameAttribute);
     }
@@ -107,5 +117,42 @@ public class OAuthService implements OAuth2UserService<OAuth2UserRequest, OAuth2
         cookieUtil.addTokenCookie(httpServletResponse, ConstantsUtil.refreshToken, refreshToken, jwtTokenProvider.getREFRESH_TOKEN_EXPIRE_TIME(), true);
         RefreshToken entityToRedis = new RefreshToken(user.getOauthId(), refreshToken, jwtTokenProvider.getREFRESH_TOKEN_EXPIRE_TIME());
         refreshTokenRepository.save(entityToRedis);
+    }
+
+    private void redirectUser(UserJpaEntity user){
+        Role role = user.getRole();
+        String userName = user.getUserName();
+
+        if (role == UNAPPROVED && userName != null) {
+            redirectPendingPage();
+        } else if (role == ADMIN) {
+            redirectHomePage();
+        } else {
+            redirectSignupPage();
+        }
+    }
+
+    private void redirectPendingPage(){
+        try {
+            httpServletResponse.sendRedirect(siteAddress + "/auth/signup/pending");
+        } catch (IOException e) {
+            log.error(siteAddress + "/auth/signup/pending 페이지로 redirect 도중 에러가 발생했습니다.", e);
+        }
+    }
+
+    private void redirectHomePage(){
+        try {
+            httpServletResponse.sendRedirect(siteAddress);
+        } catch (IOException e) {
+            log.error(siteAddress + "페이지로 redirect 도중 에러가 발생했습니다.", e);
+        }
+    }
+
+    private void redirectSignupPage(){
+        try {
+            httpServletResponse.sendRedirect(siteAddress + "/auth/signup");
+        } catch (IOException e) {
+            log.error(siteAddress + "/auth/signup 페이지로 redirect 도중 에러가 발생했습니다.", e);
+        }
     }
 }
