a755759aa19dfa0393136f81b07d7dee41293eee
신희성 <vvwv3092@gmail.com>
2024-02-22 05:01:36 +0900

    feat logout controller

--- presentation/src/main/java/team/themoment/officialgsm/admin/auth/controller/AuthController.java
+++ presentation/src/main/java/team/themoment/officialgsm/admin/auth/controller/AuthController.java
@@ -1,16 +1,20 @@
 package team.themoment.officialgsm.admin.auth.controller;
 
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import team.themoment.officialgsm.admin.auth.controller.dto.request.UserNameModifyRequest;
 import team.themoment.officialgsm.admin.auth.controller.dto.response.UserInfoResponse;
+import team.themoment.officialgsm.admin.auth.controller.manager.CookieManager;
 import team.themoment.officialgsm.admin.auth.controller.manager.UserManager;
 import team.themoment.officialgsm.admin.auth.controller.mapper.AuthDataMapper;
+import team.themoment.officialgsm.common.util.ConstantsUtil;
 import team.themoment.officialgsm.domain.auth.dto.UserInfoDto;
 import team.themoment.officialgsm.domain.auth.usecase.FindUserInfoUseCase;
+import team.themoment.officialgsm.domain.auth.usecase.LogoutUseCase;
 import team.themoment.officialgsm.domain.auth.usecase.ModifyNameUseCase;
 import team.themoment.officialgsm.domain.user.User;
 
@@ -20,10 +24,12 @@ import team.themoment.officialgsm.domain.user.User;
 @RequiredArgsConstructor
 public class AuthController {
 
-    private final FindUserInfoUseCase findUserInfoUseCase;
     private final ModifyNameUseCase modifyNameUseCase;
+    private final FindUserInfoUseCase findUserInfoUseCase;
+    private final LogoutUseCase logoutUseCase;
 
     private final UserManager userManager;
+    private final CookieManager cookieManager;
     private final AuthDataMapper userDataMapper;
 
     @PatchMapping("/username")
@@ -40,5 +46,13 @@ public class AuthController {
         User user = userManager.getCurrentUser();
         UserInfoDto userInfoDto = findUserInfoUseCase.execute(user);
         return ResponseEntity.ok(userDataMapper.toInfoResponse(userInfoDto));
+    }
+
+    @DeleteMapping("/logout")
+    public ResponseEntity<Void> logout(HttpServletRequest request) {
+        String accessToken = cookieManager.getCookieValue(request, ConstantsUtil.accessToken);
+        User user = userManager.getCurrentUser();
+        logoutUseCase.execute(accessToken, user);
+        return ResponseEntity.noContent().build();
     }
 }
