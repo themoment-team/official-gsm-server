27e8f1e0c9e480684ac56034c20a8606228e2fa3
신희성 <vvwv3092@gmail.com>
2024-02-28 02:27:03 +0900

    feat approved controller

--- presentation/src/main/java/team/themoment/officialgsm/admin/controller/approve/ApproveController.java
+++ presentation/src/main/java/team/themoment/officialgsm/admin/controller/approve/ApproveController.java
@@ -1,13 +1,15 @@
 package team.themoment.officialgsm.admin.controller.approve;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import team.themoment.officialgsm.admin.controller.approve.dto.response.UnapprovedListResponse;
 import team.themoment.officialgsm.admin.controller.approve.mapper.ApproveDataMapper;
+import team.themoment.officialgsm.admin.controller.auth.manager.UserManager;
+import team.themoment.officialgsm.domain.approve.usecase.ApprovedUseCase;
 import team.themoment.officialgsm.domain.approve.usecase.UnapprovedListUseCase;
+import team.themoment.officialgsm.domain.user.User;
 
 import java.util.List;
 
@@ -17,12 +19,21 @@ import java.util.List;
 public class ApproveController {
 
     private final UnapprovedListUseCase unapprovedListUseCase;
+    private final ApprovedUseCase approvedUseCase;
 
     private final ApproveDataMapper approveDataMapper;
+    private final UserManager userManager;
 
 
     @GetMapping("/unapproved/list")
     public ResponseEntity<List<UnapprovedListResponse>> unapprovedListFind() {
         return ResponseEntity.ok(approveDataMapper.toUnapprovedListResponse(unapprovedListUseCase.execute()));
+    }
+
+    @PatchMapping("/approved/{oauthId}")
+    public ResponseEntity<Void> approved(@PathVariable String oauthId) {
+        User grantor = userManager.getCurrentUser();
+        approvedUseCase.execute(oauthId, grantor);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 }
