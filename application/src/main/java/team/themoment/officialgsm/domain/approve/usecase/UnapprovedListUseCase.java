cc7b7f83a9d1af15d459021716d7eb1ca403d370
신희성 <vvwv3092@gmail.com>
2024-02-28 02:13:46 +0900

    feat UnapprovedListUseCase

---
+++ application/src/main/java/team/themoment/officialgsm/domain/approve/usecase/UnapprovedListUseCase.java
@@ -0,0 +1,27 @@
+package team.themoment.officialgsm.domain.approve.usecase;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import team.themoment.officialgsm.common.annotation.UseCaseWithTransaction;
+import team.themoment.officialgsm.domain.approve.dto.UnapprovedListDto;
+import team.themoment.officialgsm.domain.user.Role;
+import team.themoment.officialgsm.repository.user.UserRepository;
+
+import java.util.List;
+
+@Service
+@UseCaseWithTransaction
+@RequiredArgsConstructor
+public class UnapprovedListUseCase {
+    private final UserRepository userRepository;
+
+    public List<UnapprovedListDto> execute() {
+        return userRepository.findAllByRoleAndUserNameNotNull(Role.UNAPPROVED).stream()
+                .map(user -> UnapprovedListDto.builder()
+                        .userSeq(user.oauthId())
+                        .userName(user.userName())
+                        .role(user.role())
+                        .requestedAt(user.requestedAt())
+                        .build()).toList();
+    }
+}
