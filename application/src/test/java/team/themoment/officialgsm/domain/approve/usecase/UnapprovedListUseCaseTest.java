e46c19dc0adbc712f7ba506e4ba3a5c3920e9c26
신희성 <vvwv3092@gmail.com>
2024-03-02 17:03:12 +0900

    test UnapprovedListUseCaseTest

---
+++ application/src/test/java/team/themoment/officialgsm/domain/approve/usecase/UnapprovedListUseCaseTest.java
@@ -0,0 +1,51 @@
+package team.themoment.officialgsm.domain.approve.usecase;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import team.themoment.officialgsm.domain.approve.dto.UnapprovedListDto;
+import team.themoment.officialgsm.domain.user.Role;
+import team.themoment.officialgsm.domain.user.User;
+import team.themoment.officialgsm.repository.user.UserRepository;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class UnapprovedListUseCaseTest {
+
+    @InjectMocks
+    private UnapprovedListUseCase unapprovedListUseCase;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Test
+    void execute() {
+        // given
+        String oauthId = "0";
+        String userName = "신희성";
+        Role userRole = Role.UNAPPROVED;
+
+        List<User> userList = List.of(new User(oauthId, userName, "s23012@gsm.hs.kr", userRole, null, null, null));
+
+        given(userRepository.findAllByRoleAndUserNameNotNull(userRole)).willReturn(userList);
+
+        // when
+        List<UnapprovedListDto> result = unapprovedListUseCase.execute();
+
+        // then
+        verify(userRepository, times(1)).findAllByRoleAndUserNameNotNull(userRole);
+
+        assertThat(oauthId).isEqualTo(result.get(0).getUserSeq());
+        assertThat(userName).isEqualTo(result.get(0).getUserName());
+        assertThat(userRole).isEqualTo(result.get(0).getRole());
+
+    }
+}
