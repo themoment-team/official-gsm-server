57dad5bdce6942fa04aaccd7c0f0afd91dc6bd73
신희성 <vvwv3092@gmail.com>
2024-03-02 17:03:46 +0900

    test ApprovedUseCaseTest

---
+++ application/src/test/java/team/themoment/officialgsm/domain/approve/usecase/ApprovedUseCaseTest.java
@@ -0,0 +1,55 @@
+package team.themoment.officialgsm.domain.approve.usecase;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import team.themoment.officialgsm.domain.user.Role;
+import team.themoment.officialgsm.domain.user.User;
+import team.themoment.officialgsm.repository.user.UserRepository;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class ApprovedUseCaseTest {
+
+    @InjectMocks
+    private ApprovedUseCase approvedUseCase;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Captor
+    private ArgumentCaptor<User> userCaptor;
+
+    @Test
+    void execute() {
+        // given
+        String oauthId = "0";
+        User user = new User(oauthId, "신희성", "s23012@gsm.hs.kr", Role.UNAPPROVED, null, null, null);
+        User grantor = new User("1", "최장우", "s22000@gsm.hs.kr", Role.ADMIN, null, null, null);
+
+        given(userRepository.findByOauthId(oauthId)).willReturn(Optional.of(user));
+
+        // when
+        approvedUseCase.execute(oauthId, grantor);
+
+        // then
+        verify(userRepository, times(1)).save(userCaptor.capture());
+
+        User capturedUser = userCaptor.getValue();
+
+        assertNotNull(capturedUser);
+        assertThat(Role.ADMIN).isEqualTo(capturedUser.role());
+        assertThat(user.userName()).isEqualTo(capturedUser.userName());
+        assertThat(grantor).isEqualTo(capturedUser.grantor());
+    }
+}
