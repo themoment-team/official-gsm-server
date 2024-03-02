d8fbbc1499a0ecdce5ae845cbf0b6b1adedffb4b
신희성 <vvwv3092@gmail.com>
2024-03-02 16:26:03 +0900

    test execute_refreshTokenNotFound

--- application/src/test/java/team/themoment/officialgsm/domain/auth/usecase/LogoutUseCaseTest.java
+++ application/src/test/java/team/themoment/officialgsm/domain/auth/usecase/LogoutUseCaseTest.java
@@ -7,6 +7,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
+import team.themoment.officialgsm.common.exception.CustomException;
 import team.themoment.officialgsm.domain.token.BlackList;
 import team.themoment.officialgsm.domain.token.RefreshToken;
 import team.themoment.officialgsm.domain.user.Role;
@@ -16,6 +17,7 @@ import team.themoment.officialgsm.repository.token.RefreshTokenRepository;
 
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 
@@ -54,5 +56,23 @@ class LogoutUseCaseTest {
         // then
         verify(refreshTokenRepository, times(1)).delete(refreshToken);
         verify(blackListRepository, times(1)).save(new BlackList(oauthId, accessTokenValue, 7200L));
+    }
+
+    @Test
+    void execute_refreshTokenNotFound() {
+        // given
+        String accessTokenValue = "0";
+        String oauthId = "0";
+
+        User user = new User(oauthId, "신희성", "s23012@gsm.hs.kr", Role.UNAPPROVED, null, null, null);
+
+        given(refreshTokenRepository.findByOauthId(oauthId)).willReturn(Optional.empty());
+
+        // when
+        assertThrows(CustomException.class, () -> logoutUseCase.execute(accessTokenValue, user));
+
+        // then
+        verify(refreshTokenRepository, never()).delete(any());
+        verify(blackListRepository, never()).save(any());
     }
 }
